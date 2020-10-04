d9be2d3c13337f3865945d70cfcc5b73e4c019a6
tricky.master <ROHITMAHIPAL.CSE1711141027@gehu.ac.in>
2020-10-04 18:49:52 +0530

    second

--- src/components/Root/Counter/Counter.test.js
+++ src/components/Root/Counter/Counter.test.js
@@ -7,10 +7,10 @@ test("Counter component works properly", () => {
   const { getByTestId, getByText } = render(<Counter />);
 
   const counterValueEl = getByTestId("counter-value");
-  const minusTwoButtonEl = getByText("-2", { selector: "button" });
+  //const minusTwoButtonEl = getByText("-2", { selector: "button" });
   const minusOneButtonEl = getByText("-1", { selector: "button" });
   const plusOneButtonEl = getByText("+1", { selector: "button" });
-  const plusTwoButtonEl = getByText("+2", { selector: "button" });
+  //const plusTwoButtonEl = getByText("+2", { selector: "button" });
 
   expect(counterValueEl).toHaveTextContent("0");
 
@@ -20,11 +20,11 @@ test("Counter component works properly", () => {
 
   fireEvent.click(minusTwoButtonEl);
 
-  expect(counterValueEl).toHaveTextContent("-3");
+  expect(counterValueEl).toHaveTextContent("-2");
 
   fireEvent.click(plusOneButtonEl);
 
-  expect(counterValueEl).toHaveTextContent("-2");
+  expect(counterValueEl).toHaveTextContent("-1");
 
   fireEvent.click(plusTwoButtonEl);
 
